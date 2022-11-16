cccf9cf9b21e60b677d453334f96531b0bd6a30d
FrahOrru <francesca.orru00@outlook.it>
2022-11-16 23:10:38 +0100

    try

---
+++ src/components/CrimeList.vue
@@ -0,0 +1,24 @@
+<script setup>
+	import { ref } from 'vue'
+	const response = ref({})
+
+  // adding delay
+  await new Promise(resolve => {
+    setTimeout(() => { 
+      resolve(true)
+    }, 1000)
+  })
+    
+  try {
+    const res = await fetch('https://api.usa.gov/crime/fbi/sapi/api/data/supplemental/national/property_type/2021/2022?API_KEY=0DK9v5A3mzlvUJphIjhAeCsg2hdvsmqnLxmGP7Ko')
+    console.log(res)
+    response.value = (await res.json())
+  } catch (error) {
+    response.value = { answer: 'Error! Could not reach the API. ' + error, image: '' }
+  }
+
+</script>
+
+<template>
+  <p>{{ response}}</p>
+</template>
